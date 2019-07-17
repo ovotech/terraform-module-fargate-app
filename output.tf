a736f671e95f0a39b40c5d09835ad04314e8ad3b
Kevin Mason <kevin.mason@ovoenergy.com>
2019-07-17 10:03:01 +0100

    added outputs for loadbalance values required for creating r53 record

---
+++ output.tf
@@ -0,0 +1,16 @@
+output "alb_zone_arn" {
+  value = "${aws_alb.main.arn}"
+}
+
+output "alb_zone_id" {
+  value = "${aws_alb.main.zone_id}"
+
+}
+
+output "alb_dns_name" {
+  value = "${aws_alb.main.dns_name}"
+}
+
+output "target_group_arn" {
+  value = "${aws_alb_target_group.main.arn}"
+}
